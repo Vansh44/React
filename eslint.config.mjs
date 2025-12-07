7454ce9da6229158ecb4dc3ef3a5bb8b63bfd494
Vansh44 <iamvanshgupta608@gmail.com>
2025-12-07 12:51:59 +0530

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,13 +6,13 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    plugins: { js, react: pluginReact },
+    extends: ["js/recommended", pluginReact.configs.flat.recommended],
     languageOptions: { globals: globals.browser },
     rules: {
-      "no-undef": "error", // ⬅️ add this line
+      "no-undef": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
-
-  pluginReact.configs.flat.recommended,
 ]);
